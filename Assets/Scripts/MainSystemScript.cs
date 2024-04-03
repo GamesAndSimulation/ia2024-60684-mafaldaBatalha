af9604930ade34028d82437a7fda1bb66ec541b8
Mafalda Batalha <mf.batalha@campus.fct.unl.pt>
2024-04-03 14:16:29 +0100

    Changed the camera to cinemachine

--- Assets/Scripts/MainSystemScript.cs
+++ Assets/Scripts/MainSystemScript.cs
@@ -13,7 +13,7 @@ public class MainSystemScript : MonoBehaviour
     public float speed = 5.0f;
     public float jumpForce = 0.01f;
     float angle;
- 
+    Vector3 direction;
 
     // Start is called before the first frame update
     void Start()
@@ -26,33 +26,32 @@ public class MainSystemScript : MonoBehaviour
         float horizontalValue = Input.GetAxis("Horizontal");
         float verticalValue = Input.GetAxis("Vertical");
 
-        float horizontalMouseValue = Input.GetAxis("Mouse X");
-        float verticalMouseValue = Input.GetAxis("Mouse Y");
-
+        //if the horizontal and vertical values are zero, it means the player is just standing, therefore I want them to face the camera
         if (horizontalValue == 0 && verticalValue == 0)
-            angle = 0;
-        else
+            angle = 180;
+        else 
         {
+            //calculates the angle of the rotation of the model on the y axis based on the input of the arrow keys
              angle = (float)((180 / Math.PI) * Math.Atan(horizontalValue / verticalValue));
 
-            if (verticalValue >= 0)
-            {
+            //adjust the angle when pressing the arrow down
+            if (verticalValue < 0)
                 angle += 180;
-            }
+                      
         }
-           
+
+        
+        //rotate the model of the player on the y axis according to the direction they're walking towards
         model.transform.eulerAngles = new Vector3(0, angle, 0);
-        transform.position += transform.forward * -verticalValue * speed * Time.deltaTime + transform.right * -horizontalValue * speed * Time.deltaTime;
+        /*
+         * normalized vector that represents the direction of the player's movement (if the vector wasn't normalized, the player 
+         * would move too quickly diagonally
+         */
+        direction = new Vector3 (horizontalValue, 0, verticalValue).normalized;
+
+        transform.position += direction * speed * Time.deltaTime;
         
 
-
-        if (Input.GetButton("Jump"))
-
-            rigidbody.AddForce(new Vector3(0, jumpForce, 0));
-          
-        
-     
-        camera.transform.eulerAngles += new Vector3(-verticalMouseValue, horizontalMouseValue, 0);
 
     }
 
