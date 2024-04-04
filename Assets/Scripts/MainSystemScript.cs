792555aba5448487489c3f22a7dcc0d269f680bf
Mafalda Batalha <mf.batalha@campus.fct.unl.pt>
2024-04-04 18:31:01 +0100

    Platforms working and started working on thedisco

--- Assets/Scripts/MainSystemScript.cs
+++ Assets/Scripts/MainSystemScript.cs
@@ -2,58 +2,97 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem.XR;
 using UnityEngine.Rendering;
 using UnityEngine.UIElements;
 
 public class MainSystemScript : MonoBehaviour
 {
-    public GameObject camera;
     public GameObject model;
-    public Rigidbody rigidbody;
+    public GameObject camera;   
+    public CharacterController characterController;
     public float speed = 5.0f;
-    public float jumpForce = 0.01f;
-    float angle;
-    Vector3 direction;
+    public float jumpSpeed = 6.0f;
+
+    private float angle;
+    private float turnSmoothTime = 0.1f;
+    private float turnSmoothVelocity;
+    private float ySpeed;
+    private float originalStepOffset;
+    private Vector3 direction;
+    private Vector3 velocity;
+    private float time;
+
 
     // Start is called before the first frame update
     void Start()
     {
+        originalStepOffset = characterController.stepOffset;
     }
 
     // Update is called once per frame
     void Update()
     {
+
         float horizontalValue = Input.GetAxis("Horizontal");
         float verticalValue = Input.GetAxis("Vertical");
 
+
         //if the horizontal and vertical values are zero, it means the player is just standing, therefore I want them to face the camera
         if (horizontalValue == 0 && verticalValue == 0)
-            angle = 180;
-        else 
         {
-            //calculates the angle of the rotation of the model on the y axis based on the input of the arrow keys
-             angle = (float)((180 / Math.PI) * Math.Atan(horizontalValue / verticalValue));
+            time += Time.deltaTime;
+            if (time > 5)
+            {
+                angle = 180;
+            }
+            velocity = new Vector3(0,0,0);
 
-            //adjust the angle when pressing the arrow down
-            if (verticalValue < 0)
-                angle += 180;
-                      
+        }
+        //calculate the angle of the player depending on their direction
+        else
+        {
+            time = 0;
+            angle = Mathf.Atan2(horizontalValue, verticalValue) * Mathf.Rad2Deg + camera.transform.eulerAngles.y;
+
+            //vector that represents the direction in which the player is moving based on the input of the arrow/WASD keys and the camera
+            direction = Quaternion.Euler(0f, angle, 0f) * Vector3.forward;
+
+           
+            //the direction vector is normalized, because otherwise the player would move too quickly diagonally
+            velocity = direction.normalized * speed;
+
         }
 
-        
-        //rotate the model of the player on the y axis according to the direction they're walking towards
-        model.transform.eulerAngles = new Vector3(0, angle, 0);
-        /*
-         * normalized vector that represents the direction of the player's movement (if the vector wasn't normalized, the player 
-         * would move too quickly diagonally
-         */
-        direction = new Vector3 (horizontalValue, 0, verticalValue).normalized;
+        ySpeed += Physics.gravity.y * Time.deltaTime;
 
-        transform.position += direction * speed * Time.deltaTime;
-        
+        if (characterController.isGrounded){
+            characterController.stepOffset = originalStepOffset;
+            ySpeed = -0.5f;
 
+            if (Input.GetButton("Jump"))
+            {
+                ySpeed = jumpSpeed;
+            }
+        }
+        else
+        {
+            characterController.stepOffset = 0f;
+        }
 
+        velocity.y = ySpeed;
+
+        //move the character based on the direction of the input
+        characterController.Move(velocity * Time.deltaTime);
+
+        //applied SmoothDampAngle to the previously defined angle in order to make the turn smoother
+        angle = Mathf.SmoothDampAngle(model.transform.eulerAngles.y, angle, ref turnSmoothVelocity, turnSmoothTime);
+
+        //rotate the model of the player on the y axis according 
+        model.transform.eulerAngles = new Vector3(0f, angle, 0f);
+
+ 
     }
 
-
+   
 }
---
+++ Assets/Scripts/MovingPlatforms.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatforms : MonoBehaviour
+{
+
+    private Transform startPos, endPos;
+    public List<Transform> positions;
+    public float speed = 0.5f;
+
+    private float startTime, totalDistance;
+    private int index = 0;
+
+    //private float t; //alternative version
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startTime = Time.time;
+        if (positions.Count >= 2)
+            updateIndexes();
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float t = (Time.time - startTime) * speed;
+        //t += Time.deltaTime;  //alternative
+        if (t > 1)
+        {
+            updateIndexes();
+            t -= 1;
+            startTime = Time.time;
+        }
+        transform.position = Vector3.Lerp(startPos.position, endPos.position, t);
+        transform.rotation = Quaternion.Slerp(startPos.rotation, endPos.rotation, t);
+        //transform.localScale = Vector3.Lerp(startPos.localScale, endPos.localScale, t);
+
+    }
+    //updates the start and end positions of the lerp
+    void updateIndexes()
+    {
+        if (index == positions.Count - 1) //reached the end
+        {
+            startPos = positions[index];
+            endPos = positions[0];
+            index = 0;
+        }
+        else
+        {
+            startPos = positions[index];
+            endPos = positions[index + 1];
+            index++;
+        }
+
+    }
+
+    
+    void OnTriggerEnter(Collider other)
+    {
+
+        if (other.gameObject.tag == "Player")
+        {
+            Debug.Log("A");
+            other.transform.SetParent(transform);            
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+
+        if (other.gameObject.tag == "Player")
+        {
+            other.transform.SetParent(null);
+        }
+    }
+
+
+    
+}
